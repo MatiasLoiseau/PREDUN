df4a67e5222dcd124e3394e38b7aaaa8db943a59
MatiasLoiseau <matiasloiseau@gmail.com>
2025-02-17 21:05:46 -0300

    Fix tab

--- models/sample_model.py
+++ models/sample_model.py
@@ -52,4 +52,4 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
-    print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
