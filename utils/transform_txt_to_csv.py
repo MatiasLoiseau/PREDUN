677bd541240f0b0326865ba4367e4d38f0ba1001
MatiasLoiseau <matiasloiseau@gmail.com>
2025-07-11 13:56:37 -0300

    Minor fix transforming txt to csv file

--- utils/transform_txt_to_csv.py
+++ utils/transform_txt_to_csv.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
-input_path = "data-private/2025_1C/porcentaje_avance_20250512.txt"
-output_path = "data-private/2025_1C/porcentaje_avance_20250512.csv"
+input_path = "data-private/2025_1C/raw/porcentaje_avance_20250512.txt"
+output_path = "data-private/2025_1C/raw/porcentaje_avance_20250512.csv"
 
 df = pd.read_csv(input_path, sep="|", engine="python")
+
+if 'Unnamed: 18' in df.columns:
+    df = df.drop(columns=['Unnamed: 18'])
+
 df.to_csv(output_path, index=False)
 
 print(f"File saved as: {output_path}")
