73c704c4fcbe9c843fb92e33ee904bc19802370f
MatiasLoiseau <matiasloiseau@gmail.com>
2025-07-28 19:11:00 -0300

    Fix dbt_assets definition

--- predun_dagster/predun_dagster/assets/dbt_assets.py
+++ predun_dagster/predun_dagster/assets/dbt_assets.py
@@ -3,7 +3,8 @@ from ..constants import DBT_PROJECT_DIR
 
 MANIFEST_PATH = DBT_PROJECT_DIR / "target" / "manifest.json"
 
-@dbt_assets(manifest=str(MANIFEST_PATH))
+@dbt_assets(manifest=str(MANIFEST_PATH), required_resource_keys={"dbt"})
 def dbt_project_assets(context):
-    yield from context.resources.dbt.cli(["run"],  context=context)
-    yield from context.resources.dbt.cli(["test"], context=context)
+    yield from context.resources.dbt.cli([
+        "run", "--select", "canonical.*"
+    ], context=context).stream()
